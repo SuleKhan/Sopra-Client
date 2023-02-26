3060aa17637b7337302f5d8e9685cb60bb7dffb2
Suleman <sulekhan111@gmail.com>
2023-02-26 17:10:47 +0100

    changed name to password in Login.js

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -37,12 +37,12 @@ FormField.propTypes = {
 
 const Login = props => {
   const history = useHistory();
-  const [name, setName] = useState(null);
+  const [password, setPassword] = useState(null);
   const [username, setUsername] = useState(null);
 
   const doLogin = async () => {
     try {
-      const requestBody = JSON.stringify({username, name});
+      const requestBody = JSON.stringify({username, password});
       const response = await api.post('/users', requestBody);
 
       // Get the returned user and update a new object.
@@ -68,9 +68,9 @@ const Login = props => {
             onChange={un => setUsername(un)}
           />
           <FormField
-            label="Name"
-            value={name}
-            onChange={n => setName(n)}
+            label="Password"
+            value={password}
+            onChange={p => setPassword(p)}
           />
           <div className="login button-container">
             <Button
@@ -80,7 +80,7 @@ const Login = props => {
               Register
             </Button>
             <Button
-              disabled={!username || !name}
+              disabled={!username || !password}
               width="100%"
               onClick={() => doLogin()}
             >
